6bdbc3b020068679dd507a4b2a9f845d13a423e3
ashiqursami <2020741@iub.edu.bd>
2023-12-01 00:44:20 +0600

    modified

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "100mb" }));
 
-
-// mongodb+srv://samitaseen:2020741@products.rjrwwqd.mongodb.net/
 //env
 const dotEnv=require("dotenv");
 dotEnv.config();
